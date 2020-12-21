1ec2497f0d6bb25f76af35329b653f6970e52c6a
Sandesh Shetty <sandeshshetty95@gmail.com>
2020-12-22 00:18:51 +0530

    Added search box

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Card from './Card';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import { robots } from './robots';
 
 const App = (props) => {
-    return (<>
-        <h1>RoboFriends</h1>
-        <CardList robots={robots} />
-    </>);
+    return (
+        <div className='tc'>
+            <h1>RoboFriends</h1>
+            <SearchBox />
+            <CardList robots={robots} />
+        </div>
+    );
 };
 
 export default App;
