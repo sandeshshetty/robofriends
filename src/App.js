a4c1ccabf786b6dc96200690f4d56029d32371df
Sandesh Shetty <sandeshshetty95@gmail.com>
2020-12-22 10:26:43 +0530

    Obtain user list from the internet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 // Use class mode instead of method to use state
@@ -11,9 +10,16 @@ class App extends Component {
         // Always call super before accessing this
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         };
+    }
+
+    // Since this is part of react, arrow function not mandatory
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}))
     }
 
     // Use arrow operator for this reference in event callbacks
@@ -27,10 +33,12 @@ class App extends Component {
             return robot.name.toLowerCase().includes(
                 this.state.searchField.toLowerCase());
         });
+        
         return (
             <div className='tc'>
                 <h1 className='ttu f2'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
+                {this.state.robots.length === 0 && <h2>Loading</h2>}
                 <CardList robots={filteredRobots} />
             </div>
         );
